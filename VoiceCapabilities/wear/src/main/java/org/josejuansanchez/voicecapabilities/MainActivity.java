d2c083067517af6476ebb736703a8de80b61dd8d
José Juan Sánchez <josejuan.sanchez@gmail.com>
2015-03-17 04:45:59 +0100

    A constant has been declared as 'static final'

--- VoiceCapabilities/wear/src/main/java/org/josejuansanchez/voicecapabilities/MainActivity.java
+++ VoiceCapabilities/wear/src/main/java/org/josejuansanchez/voicecapabilities/MainActivity.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class MainActivity extends Activity {
 
-    private final String TAG = "Wear-MainActivity";
+    private static final String TAG = "Wear-MainActivity";
     private Button mButton;
 
     @Override
